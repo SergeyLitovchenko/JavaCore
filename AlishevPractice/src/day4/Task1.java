d93d95812841cbc54de64e7618a6093f1d60f62e
SergeyLitovchenko <magicwanderer9@gmail.com>
2022-05-13 00:47:06 +0300

    finish day4 task1

--- AlishevPractice/src/day4/Task1.java
+++ AlishevPractice/src/day4/Task1.java
@@ -4,21 +4,54 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Задайте размер массива:");
+        System.out.println("Введите число:");
         Scanner input = new Scanner(System.in);
-
         int size = input.nextInt();
+        input.close();
+
         int[] numbers = new int[size];
+        int numbersBigger8 = 0;
+        int numbers1 = 0;
+        int numbersEven = 0;
+        int numbersUneven = 0;
+        int sum = 0;
+
 
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = (int) Math.ceil(Math.random() * 10);
         }
-        System.out.println("Введено число " + size +". Сгенерирован следующий массив:");
+
+        System.out.println("Введено число " + size + ". Сгенерирован следующий массив:");
+
         for (int i = 0; i < numbers.length; i++) {
             System.out.print(numbers[i] + " ");
+
+            sum +=numbers[i];
+
+            if (numbers[i] > 8) {
+                numbersBigger8++;
+            }
+            if (numbers[i] == 1) {
+                numbers1++;
+            }
+            if (numbers[i] % 2 == 0) {
+                numbersEven++;
+            } else {
+                numbersUneven++;
+            }
         }
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("Информация о массиве:");
+        System.out.println("Длина массива: " + size);
+        System.out.println("Количестве чисел больше 8: " + numbersBigger8);
+        System.out.println("Количестве чисел равных 1: " + numbers1);
+        System.out.println("Количестве четных чисел: " + numbersEven);
+        System.out.println("Количестве нечетных чисел: " + numbersUneven);
+        System.out.println("Сумма всех элементов массива: " + sum);
     }
 }
+
 //    С клавиатуры вводится число n - размер массива. Необходимо создать массив
 //    указанного размера и заполнить его случайными числами от 0 до 10. Затем вывести
 //    содержимое массива в консоль, а также вывести в консоль информацию о:
