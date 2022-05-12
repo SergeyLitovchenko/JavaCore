284bc9ca91db5f106cc777826794fb74d42b53a0
SergeyLitovchenko <magicwanderer9@gmail.com>
2022-05-13 01:47:31 +0300

    refactor day4 task1

--- AlishevPractice/src/day4/Task1.java
+++ AlishevPractice/src/day4/Task1.java
@@ -1,53 +1,51 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Введите число:");
         Scanner input = new Scanner(System.in);
-        int size = input.nextInt();
+        int arrayLenght = input.nextInt();
         input.close();
 
-        int[] numbers = new int[size];
-        int numbersBigger8 = 0;
-        int numbers1 = 0;
-        int numbersEven = 0;
-        int numbersUneven = 0;
+        int counter8 = 0;
+        int counter1 = 0;
+        int counterEven = 0;
+        int counterOdd = 0;
         int sum = 0;
 
+        Random random = new Random();
+        int[] array = new int[arrayLenght];
 
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = (int) Math.ceil(Math.random() * 10);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10);
         }
 
-        System.out.println("Введено число " + size + ". Сгенерирован следующий массив:");
+        System.out.println("Введено число " + arrayLenght + ". Сгенерирован следующий массив:");
+        System.out.println(Arrays.toString(array));
 
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
+        for (int i = 0; i < array.length; i++) {
+            sum +=array[i];
 
-            sum +=numbers[i];
-
-            if (numbers[i] > 8) {
-                numbersBigger8++;
-            }
-            if (numbers[i] == 1) {
-                numbers1++;
-            }
-            if (numbers[i] % 2 == 0) {
-                numbersEven++;
-            } else {
-                numbersUneven++;
-            }
+            if (array[i] > 8)
+                counter8++;
+            if (array[i] == 1)
+                counter1++;
+            if (array[i] % 2 == 0)
+                counterEven++;
+             else
+                counterOdd++;
         }
-        System.out.println(" ");
         System.out.println(" ");
         System.out.println("Информация о массиве:");
-        System.out.println("Длина массива: " + size);
-        System.out.println("Количестве чисел больше 8: " + numbersBigger8);
-        System.out.println("Количестве чисел равных 1: " + numbers1);
-        System.out.println("Количестве четных чисел: " + numbersEven);
-        System.out.println("Количестве нечетных чисел: " + numbersUneven);
+        System.out.println("Длина массива: " + array.length);
+        System.out.println("Количестве чисел больше 8: " + counter8);
+        System.out.println("Количестве чисел равных 1: " + counter1);
+        System.out.println("Количестве четных чисел: " + counterEven);
+        System.out.println("Количестве нечетных чисел: " + counterOdd);
         System.out.println("Сумма всех элементов массива: " + sum);
     }
 }
