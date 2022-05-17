f2a0deb7c8b945099ca84457b01932ccba723f44
SergeyLitovchenko <magicwanderer9@gmail.com>
2022-05-17 19:14:23 +0300

    refactor day5

--- AlishevPractice/src/day5/Task2.java
+++ AlishevPractice/src/day5/Task2.java
@@ -2,36 +2,14 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-        Motorbike motorbike1 = new Motorbike(2020,"red","rocket");
+        Motorbike motorbike1 = new Motorbike(2020, "rocket", "red");
 
-        System.out.println("Год выпуска автомобиля: " + motorbike1.getYear());
-        System.out.println("Цвет: " + motorbike1.getColor());
-        System.out.println("Модель: " + motorbike1.getModel());
+        System.out.println("Год выпуска мотоцикла: " + motorbike1.getYear() + "\n" +
+                "Модель: " + motorbike1.getModel() + "\n" +
+                "Цвет: " + motorbike1.getColor());
     }
 }
 
-class Motorbike {
-    private int year;
-    private String color;
-    private String model;
-
-    public Motorbike (int year, String color, String model) {
-        this.year = year;
-        this.color = color;
-        this.model = model;
-    }
-    public int getYear() {
-        return year;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public String getModel() {
-        return model;
-    }
-}
 
 //   Создать класс Мотоцикл, с полями “Год выпуска”, “Цвет”,“Модель”.
 //   Создать экземпляр класса Мотоцикл, с помощью конструктора (сеттеры не
