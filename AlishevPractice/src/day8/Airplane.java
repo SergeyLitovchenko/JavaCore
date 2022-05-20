b127554cc4eddfcaa3e79aa5ef258477366f4d98
SergeyLitovchenko <magicwanderer9@gmail.com>
2022-05-20 23:29:36 +0300

    add day8 task2

---
+++ AlishevPractice/src/day8/Airplane.java
@@ -0,0 +1,79 @@
+package day8;
+
+public class Airplane {
+    private String producer;
+    private int year;
+    private int length;
+    private int weight;
+    private int fuel;
+
+    public Airplane(String producer, int year, int length, int weight) {
+        this.producer = producer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+        this.fuel = 0;
+    }
+
+    public void info() {
+        System.out.println("Producer: " + producer + "\n" +
+                "Year: " + year + "\n" +
+                "Length: " + length + "\n" +
+                "Weight: " + weight + "\n" +
+                "Fuel: " + fuel);
+    }
+
+    @Override
+    public String toString() {
+        return "Producer: " + producer + "\n" +
+                "Year: " + year + "\n" +
+                "Length: " + length + "\n" +
+                "Weight: " + weight + "\n" +
+                "Fuel: " + fuel;
+    }
+
+    public void fillUp(int inputFuel) {
+        fuel += inputFuel;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+}
+
