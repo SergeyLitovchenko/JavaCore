221dc3e2fe97e8ced69a64f506279d5dba31343f
SergeyLitovchenko <magicwanderer9@gmail.com>
2022-05-21 23:53:42 +0300

    begin day9 task1

---
+++ AlishevPractice/src/day9/Human.java
@@ -0,0 +1,21 @@
+package day9;
+
+public class Human {
+    private String name;
+
+    public Human(String name) {
+        this.name = name;
+    }
+
+    public void printInfo(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
