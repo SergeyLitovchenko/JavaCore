f510e86a16f78d40fbe4f5c7a06492e1b01e440f
SergeyLitovchenko <magicwanderer9@gmail.com>
2022-05-23 23:56:35 +0300

    begin day9 task2

---
+++ AlishevPractice/src/day9/task2/Rectangle.java
@@ -0,0 +1,18 @@
+package day9.task2;
+
+public class Rectangle extends Figure{
+
+    public Rectangle(String color) {
+        super(color);
+    }
+
+    @Override
+    public double area() {
+        return 0;
+    }
+
+    @Override
+    public double perimeter() {
+        return 0;
+    }
+}
---
+++ AlishevPractice/src/day9/task2/Triangle.java
@@ -0,0 +1,18 @@
+package day9.task2;
+
+public class Triangle extends Figure{
+
+    public Triangle(String color) {
+        super(color);
+    }
+
+    @Override
+    public double area() {
+        return 0;
+    }
+
+    @Override
+    public double perimeter() {
+        return 0;
+    }
+}
