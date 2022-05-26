448699220a4dc8cb46cb41f160075ac31b620959
SergeyLitovchenko <magicwanderer9@gmail.com>
2022-05-26 23:58:31 +0300

    update day11 task1

--- AlishevPractice/src/day11/Courier.java
+++ AlishevPractice/src/day11/Courier.java
@@ -1,4 +1,33 @@
 package day11;
 
-public class Courier {
+public class Courier implements Worker {
+    private int salary;
+    private Warehouse warehouse;
+
+    public Courier(Warehouse warehouse) {
+        this.warehouse = warehouse;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public void doWork() {
+        salary += 100;
+       warehouse.setBalance(warehouse.getBalance() + 1000);
+
+    }
+
+    @Override
+    public void bonus() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Courier{" +
+                "salary=" + salary +
+                '}';
+    }
 }
