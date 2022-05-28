de1de1e0944c902b44a4c285d5c9d98292fb39c3
SergeyLitovchenko <magicwanderer9@gmail.com>
2022-05-28 23:59:33 +0300

    add day12 task1

--- AlishevPractice/src/day12/Task1.java
+++ AlishevPractice/src/day12/Task1.java
@@ -1,4 +1,29 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("Audi");
+        list.add("BMW");
+        list.add("Toyota");
+        list.add("Tesla");
+        list.add("Nissan");
+
+        System.out.println(list);
+
+        list.add(2,"Mercedes");
+        System.out.println(list);
+
+        list.remove(0);
+        System.out.println(list);
+    }
 }
+/**
+ * 1. Создать список строк, добавить в него 5 марок автомобилей, вывести список в
+ * консоль. Добавить в середину еще 1 автомобиль, удалить самый первый автомобиль
+ * из списка. Распечатать список.
+ */
